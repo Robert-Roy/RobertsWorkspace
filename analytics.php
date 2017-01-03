6f3921d17ee16e36ac946eacb6030d78f41e0d16
Robert-Roy <robertproy@live.com>
2017-01-02 18:32:49 -0600

    Change comments

--- analytics.php
+++ analytics.php
@@ -1,15 +1,3 @@
-<!--
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-89608698-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
--->
 <?php
 $page = $_SERVER['PHP_SELF'];
 $time = date("Y-m-d H:i:s");
@@ -23,7 +11,6 @@ if (is_object($array)) {
         $regionName = htmlspecialchars(\filter_var(\trim($array->regionName), FILTER_SANITIZE_STRING));
         $city = htmlspecialchars(\filter_var(\trim($array->city), FILTER_SANITIZE_STRING));
         $org = htmlspecialchars(\filter_var(\trim($array->org), FILTER_SANITIZE_STRING));
-        //$query= htmlspecialchars(\filter_var(\trim($array->query), FILTER_SANITIZE_STRING));
         mail('robertproy@live.com', "Pageload Logged", "$IP loaded $page at $time
         
 Information about this ip:
