543f4c9b22e0c2f3caa78f5ed8def2e02965cb48
Robert-Roy <robertproy@live.com>
2017-01-03 22:37:03 -0600

    Add github.php page which logs redirects to githu

---
+++ github.php
@@ -0,0 +1,9 @@
+<?php
+//It would be possible to use post to have several different redirects in one
+//file.
+header('Location: https://github.com/robert-roy');
+include_once "analytics.php"
+?>
+<?php 
+  exit;
+?>
