da076cfbac031d4907731e59f68a22b63f58acd1
Robert-Roy <robertproy@live.com>
2017-01-07 19:39:45 -0600

    add pageview logging

--- analytics.php
+++ analytics.php
@@ -3,7 +3,7 @@
 include_once "util.php";
 $util = new util();
 $conn = $util->getConn();
-$page = $_SERVER['PHP_SELF'];
+$page = htmlspecialchars(\filter_var(\trim($_SERVER['SERVER_SELF']), FILTER_SANITIZE_STRING));
 $time = date("Y-m-d H:i:s");
 $IP = htmlspecialchars(\filter_var(\trim($_SERVER['REMOTE_ADDR']), FILTER_SANITIZE_STRING));
 $json = file_get_contents("http://ip-api.com/json/$IP");
@@ -16,19 +16,27 @@ if (is_object($array)) {
         $regionName = htmlspecialchars(\filter_var(\trim($array->regionName), FILTER_SANITIZE_STRING));
         $city = htmlspecialchars(\filter_var(\trim($array->city), FILTER_SANITIZE_STRING));
         $org = htmlspecialchars(\filter_var(\trim($array->org), FILTER_SANITIZE_STRING));
-        try{
+        try {
             //Check if IP is unique, add to unique IP table if so.
             $conn->prepare('SELECT ID FROM UniqueIPs where IP = ?');
             $result = $conn->execute([$IP]);
             $instance = false;
             foreach ($result as $row) {
                 $instance = true;
+                break;
             }
             if ($instance === false) {
                 $test->prepare("INSERT INTO UniqueIPs (IP, COUNTRY, STATE, CITY, ORGANIZATION) VALUES (?, ?, ?, ?, ?)");
                 $test->execute([$IP, $country, $regionName, $city, $org]);
             }
-            } catch (Exception $ex) {
+        } catch (Exception $ex) {
+            util::handleerror("Analytics failure code 2 at $time");
+        }
+        try {
+            //Log Pageview
+            $conn->prepare('INSERT INTO PageViews VALUES (?, ?, NOW())');
+            $conn->execute([$IP, $page]);
+        } catch (Exception $ex) {
             util::handleerror("Analytics failure code 2 at $time");
         }
     } else {
