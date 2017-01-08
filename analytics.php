de02392fbe2959c54c2c64d3c82a601eb429234d
Robert-Roy <robertproy@live.com>
2017-01-07 19:29:04 -0600

    log analytics.php data to mysql

--- analytics.php
+++ analytics.php
@@ -1,6 +1,8 @@
 <?php
+
 include_once "util.php";
-//todo Log This Serverside
+$util = new util();
+$conn = $util->getConn();
 $page = $_SERVER['PHP_SELF'];
 $time = date("Y-m-d H:i:s");
 $IP = htmlspecialchars(\filter_var(\trim($_SERVER['REMOTE_ADDR']), FILTER_SANITIZE_STRING));
@@ -9,22 +11,30 @@ $array = json_decode($json);
 if (is_object($array)) {
     $status = htmlspecialchars(\filter_var(\trim($array->status), FILTER_SANITIZE_STRING));
     if ($status === "success") {
+        //Clean Up Text
         $country = htmlspecialchars(\filter_var(\trim($array->country), FILTER_SANITIZE_STRING));
         $regionName = htmlspecialchars(\filter_var(\trim($array->regionName), FILTER_SANITIZE_STRING));
         $city = htmlspecialchars(\filter_var(\trim($array->city), FILTER_SANITIZE_STRING));
         $org = htmlspecialchars(\filter_var(\trim($array->org), FILTER_SANITIZE_STRING));
-        util::mailadmin("Pageload Logged", "$IP loaded $page at $time
-        
-Information about this ip:
-    Country: $country
-    City: $city
-    Region: $regionName
-    Organization: $org
-    ");
+        try{
+            //Check if IP is unique, add to unique IP table if so.
+            $conn->prepare('SELECT ID FROM UniqueIPs where IP = ?');
+            $result = $conn->execute([$IP]);
+            $instance = false;
+            foreach ($result as $row) {
+                $instance = true;
+            }
+            if ($instance === false) {
+                $test->prepare("INSERT INTO UniqueIPs (IP, COUNTRY, STATE, CITY, ORGANIZATION) VALUES (?, ?, ?, ?, ?)");
+                $test->execute([$IP, $country, $regionName, $city, $org]);
+            }
+            } catch (Exception $ex) {
+            util::handleerror("Analytics failure code 2 at $time");
+        }
     } else {
         util::handleerror("Analytics failed for some reason at $time for $IP CODE 2"); //TODO Log error locally
     }
 } else {
-    util::handleerror( "Analytics failed for some reason at $time for $IP CODE 1"); //TODO Log error locally
+    util::handleerror("Analytics failed for some reason at $time for $IP CODE 1"); //TODO Log error locally
 }
 ?>
