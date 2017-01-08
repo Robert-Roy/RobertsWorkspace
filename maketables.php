8c1cd6bc14d143516bc37c0386fc4e5be71fa663
Robert-Roy <robertproy@live.com>
2017-01-07 19:30:16 -0600

    add maketables.php to initialize sql tables

---
+++ maketables.php
@@ -0,0 +1,36 @@
+<?php
+
+/* 
+ * All rights reserved. Copyright Robert Roy 2016.
+ */
+
+include_once "util.php";
+$util = new util();
+$conn = $util->getConn();
+//Initialize SQL Tables
+try{
+    $util->query("CREATE TABLE UniqueIPs ("
+            . "ID INT NOT NULL AUTO_INCREMENT, "
+            . "IP TEXT, "
+            . "COUNTRY TEXT, "
+            . "STATE TEXT, "
+            . "CITY TEXT, "
+            . "ORGANIZATION TEXT, "
+            . "PRIMARY KEY (ID)"
+            . ")");
+} catch (Exception $ex) {
+    //Do nothing
+    echo "ERROR ERROR ERROR ERROR eeg";
+}
+try{
+    $util->query("CREATE TABLE PageViews ("
+        . "IP TEXT, "
+        . "PAGE TEXT, "
+        . "TIME TIMESTAMP"
+        . ")");
+} catch (Exception $ex) {
+    //Do nothing
+    echo "ERROR ERROR ERROR ERROR";
+}
+
+?>
