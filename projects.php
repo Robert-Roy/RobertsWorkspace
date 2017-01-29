1e6cb74d8a6db5925758ad8e52114219ea38fc39
Robert-Roy <robertproy@live.com>
2017-01-28 20:07:33 -0600

    update project display

--- projects.php
+++ projects.php
@@ -12,28 +12,35 @@ util::printheader("My Projects");
 class Project {
 
     public $href;
+    public $githublink;
     public $title;
     public $description;
 
-    public function __construct($href, $title, $description) {
+    public function __construct($href, $githublink, $title, $description) {
+        // A blank github link may be passed if no github link is available.
         $this->href = $href;
+        $this->githublink = $githublink;
         $this->title = $title;
         $this->description = $description;
     }
 
     public function printHTML() {
         ?>
-        <div class="contentdiv projectcontainer">
-            <a class='flexFull' href="<?= $this->href ?>">
-                <div class='imagediv'>
-                    <div class="projectsquare">
-                        <div class='contentdiv projectimage'>
-                            <div class='flexFull'><h3><?= $this->title ?></h3></div>
-                            <p><?= $this->description ?></p>
-                        </div>
-                    </div>
-                </div>
-            </a>
+        <div class="projectcontainer contentdiv">
+            <h2><?= $this->title ?></h2>
+            <div class="projectdescription">
+                <?= $this->description ?>
+            </div>
+        </div>
+        <div class="crispbutton"><a href=<?= $this->href ?>>See Project</a></div>
+        <?php
+        if ($this->githublink !== "") {
+            ?>
+
+            <div class="crispbutton"><a  href="<?= $this->githublink ?>">See Code</a>
+                <?php
+            }
+            ?>
         </div>
         <?php
     }
@@ -46,29 +53,29 @@ class Project {
     // I don't like the way these are formatted, but it's a small price to pay for
     // autoformatting elsewhere.
     $projectRobertsWorkspace = new Project(
-            util::$GITHUB, "RobertsWorkspace.com", "This website was created to showcase my programming. 
-            Code is available on Github. Click here to see it.");
+            util::$HOME, "https://github.com/Robert-Roy/RobertsWorkspace", "RobertsWorkspace.com", "This website was created to showcase my programming. 
+            Code is available on Github.");
     $projectMyMoneyFees = new Project(
-            "http://web.archive.org/web/20160312232628/http://mymoneyfees.com/", "MyMoneyFees.com", "I did substantial work on this website before I had
+            "http://web.archive.org/web/20160312232628/http://mymoneyfees.com/", "", "MyMoneyFees.com", "I did substantial work on this website before I had
             researched responsive design. The website was designed
             on a WordPress platform. The current version has been heavily
             modified since then. An archived version is linked here courtesy
             of archive.org.");
     $projectNumbersIntoWords = new Project(
-            "http://www.numbersintowords.com", "NumbersIntoWords.com", "This website takes (almost) any numerical input and outputs it into"
-            . "words. (10<sup>3007</sup>-1 to -10<sup>3007</sup>+1 with support"
-            . "for up to 3006 characters after the decimal!");
+            "http://www.numbersintowords.com", "https://github.com/Robert-Roy/numbersintowords.com", "NumbersIntoWords.com", "This website takes (almost) any numerical input and outputs it into "
+            . "words. (10<sup>3007</sup>-1 to -10<sup>3007</sup>+1 with support "
+            . "for up to 3006 characters after the decimal.");
     $projectClockPuncher = new Project(
-            "http://clockpuncher.robertsworkspace.com", "Clock Puncher", "A website I'm collaborating with some friedns on. So far it doesn't"
+            "http://clockpuncher.robertsworkspace.com", "https://github.com/Robert-Roy/clockpuncher", "Clock Puncher", "A website I'm collaborating with some friedns on. So far it doesn't "
             . "do anything but handle login sessions.");
     $projectLightSpeed = new Project(
-            "http://lightspeed.robertsworkspace.com", "Light Speed!", "A javascript animation that looks like you're cruising through space."
+            "http://lightspeed.robertsworkspace.com", "https://github.com/Robert-Roy/lightspeed", "Light Speed!", "A javascript animation that looks like you're cruising through space. "
             . "Check it out!");
     $projectAnalytics = new Project(
-            util::$ROBERTSANALYTICS, "Robert's Analytics", "This page displays analytics information gathered on this website.");
+            util::$ROBERTSANALYTICS, "https://github.com/Robert-Roy/RobertsWorkspace/blob/master/dashboard.php", "Robert's Analytics", "This page displays analytics information gathered on this website.");
     $projectIPData = new Project(
-            util::$IPDATA, "About You", "I've been playing around with some IP geolocating APIs. If you're"
-            . "curious what websites can see about you, click here!");
+            util::$IPDATA, "https://github.com/Robert-Roy/RobertsWorkspace/blob/master/ipdata.php", "About You", "I've been playing around with some IP geolocating APIs. If you're "
+            . "curious what websites can see about you, check it out.");
     $projectRobertsWorkspace->printHTML();
     $projectMyMoneyFees->printHTML();
     $projectNumbersIntoWords->printHTML();
@@ -77,33 +84,6 @@ class Project {
     $projectAnalytics->printHTML();
     $projectIPData->printHTML();
     ?>
-    <div class="contentdiv projectcontainer">
-        <a class='flexFull' href="<?= util::$ROBERTSANALYTICS ?>">
-            <div class='imagediv'>
-                <div class="projectsquare">
-                    <div class='contentdiv projectimage'>
-                        <div class='flexFull'><h3>Robert's Analytics</h3></div>
-                        <p>This page displays analytics information gathered on this website.
-                            It is going to supply more information in the very near future.</p>
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-    <div class="contentdiv projectcontainer">
-        <a class='flexFull' href="<?= util::$IPDATA ?>">
-            <div class='imagediv'>
-                <div class="projectsquare">
-                    <div class='contentdiv projectimage'>
-                        <div class='flexFull'><h3>About You</h3></div>
-                        <p>I've been playing around with some IP geolocating APIs.
-                            If you're curious what websites can see about you,
-                            click here!</p>
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
 </div>
 <?php
 util::printfooter();
