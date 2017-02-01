466c39818dee1920dbc7de550686f28a8f131220
Robert-Roy <robertproy@live.com>
2017-01-31 21:44:13 -0600

    add spacy animation to website pages

--- anim.js
+++ anim.js
@@ -16,6 +16,13 @@ $(document).ready(function () {
     var context = canvas.getContext("2d");
     var W = $canvas.width();
     var H = $canvas.height();
+    
+    var red = 13;
+    var green = 110;
+    var blue = 110;
+    blue = Math.round(Math.random() * 100 + 50);
+    green = Math.round(Math.random() * 100 + 50);
+    red = Math.round(Math.random() * 100 + 50);
     canvas.width = W;
     canvas.height = H;
     var mp = 250;
@@ -23,10 +30,24 @@ $(document).ready(function () {
     for (var i = 0; i < mp; i++) {
         particles.push(makeParticle());
     }
-
+    
     function draw() {
         context.clearRect(0, 0, W, H);
-        context.fillStyle = "rgba(13, 48, 110, 1)";
+        // drift colors
+        if (red + green + blue > 300) {
+            red += Math.round(-1 * Math.random());
+            green += Math.round(-1 * Math.random());
+            blue += Math.round(-1 * Math.random());
+        } else if (red + green + blue < 200) {
+            red += Math.round(Math.random());
+            green += Math.round(Math.random());
+            blue += Math.round(Math.random());
+        } else {
+            red += Math.round(Math.random() * 2 - 1);
+            green += Math.round(Math.random() * 2 - 1);
+            blue += Math.round(Math.random() * 2 - 1);
+        }
+        context.fillStyle = "rgba(" + red + "," + green + "," + blue + ", 1)";
         context.beginPath();
         for (var i = 0; i < particles.length; i++) {
             var p = particles[i];
@@ -49,7 +70,15 @@ $(document).ready(function () {
             p.x += p.mx;
             p.y += p.my;
             p.a = Math.random() * (Math.PI * 2); // angle
-            if (p.x > W + p.r || p.x < 0 - p.r || p.y > H + p.r || p.y < 0 - p.r) {
+            if (p.x > W + p.r) {
+                p.x = 0 - p.r;
+            } else if (p.x < 0 - p.r) {
+                p.x = W + p.r;
+            } else if (p.y > H + p.r) {
+                p.y = 0 - p.r;
+            } else if (p.y < 0 - p.r) {
+                p.y = H + p.r;
+            } else if (false) {
                 particles.splice(i, 1);
                 i--;
             }
@@ -62,9 +91,9 @@ $(document).ready(function () {
         return {
             x: Math.random() * W, //start X
             y: Math.random() * H, //start Y
-            mx: Math.random() * .25 -.125, //momentum x
-            my: Math.random() * .25 -.125, //momentum y
-            r: Math.random() * 2 + 2, //radius
+            mx: Math.random() * .25 - .125, //momentum x
+            my: Math.random() * .25 - .125, //momentum y
+            r: Math.random() + 1, //radius
             d: Math.random() * 25 + 2, //density
             a: Math.random() * (Math.PI * 2) // angle
         };
