9b01b6cd086782a4f6b3fbff33081f88ea8b386e
Robert-Roy <robertproy@live.com>
2017-02-18 15:06:22 -0600

    add config.example.php

--- robertsworkspace/helpers/config.example.php
+++ robertsworkspace/helpers/config.example.php
@@ -2,9 +2,9 @@
 
 $CONFIG = [
     'database' => [
-        'name' => 'robertsw_DB',
-        'username' => 'clockpuncher_dbuser',
-        'password' => 'HTw9fr9q3Zrau7Jx',
+        'name' => 'yourDB',
+        'username' => 'yourUsername',
+        'password' => 'yourPassword',
         'connection' => 'mysql:host=localhost',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
