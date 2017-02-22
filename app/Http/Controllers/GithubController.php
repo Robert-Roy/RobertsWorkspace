6e87065bcce346842791679b607971461196432e
Robert-Roy <robertproy@live.com>
2017-02-21 21:48:23 -0600

    fix analytics bug

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -8,8 +8,8 @@ use App\Analytics;
 class GithubController extends Controller {
 
     public function index() {
-        $analytics = new Analytics("github");
-        $analytics->recordView();
+        $analytics = new Analytics();
+        $analytics->recordView("github");
         //It would be possible to use post to have several different redirects in one
         //file.
         header('Location: https://github.com/robert-roy');
--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -14,8 +14,8 @@ class ProjectsController extends Controller {
         //this is NOT the right way to implement this. Will improve after laravel refactor
 
         $title = "My Projects";
-        $analytics = new Analytics("projects");
-        $analytics->recordView();
+        $analytics = new Analytics();
+        $analytics->recordView("projects");
 
         echo view("partials.projectstop", ["title" => $title]);
         $projectRobertsWorkspace = new Project(
