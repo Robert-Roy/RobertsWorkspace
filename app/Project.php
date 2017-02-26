0956b67e8c443996d822b50b29beaa4a1911ca05
Robert-Roy <robertproy@live.com>
2017-02-25 18:46:20 -0600

    prepare Project to implement eloquent

--- app/Project.php
+++ app/Project.php
@@ -2,29 +2,9 @@
 
 namespace App;
 
-class Project{
+use Illuminate\Database\Eloquent\Model;
 
-    public $href;
-    public $githublink;
-    public $title;
-    public $description;
-
-    public function __construct($href, $githublink, $title, $description) {
-        // A blank github link may be passed if no github link is available.
-        $this->href = $href;
-        $this->githublink = $githublink;
-        $this->title = $title;
-        $this->description = $description;
-    }
-
-
-    public function getViewData() {
-        return[
-            "href" => $this->href,
-            "githublink" => $this->githublink,
-            "title" => $this->title,
-            "description" => $this->description
-        ];
-    }
-
+class Project extends Model
+{
+    //
 }
---
+++ database/migrations/2017_02_26_003643_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('active');
+            $table->integer('priority'); // will be used to determine how high something is displayed on the list
+            $table->string('projectlink')->nullable();
+            $table->string('codelink')->nullable();
+            $table->string('title');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
