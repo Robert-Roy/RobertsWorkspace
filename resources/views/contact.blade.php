671d10d7f7c5a0f7bc45797a214ff265e2781eac
Robert-Roy <robertproy@live.com>
2017-03-05 19:13:11 -0600

    make form fields required

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,13 +5,13 @@
 <div class="contentdiv contactdiv">  
     <form action="sendmail" method="post" onsubmit="return validateContactForm();">
         <label class="label" for="name">What is your name?</label>
-        <input type="text" name="name" autofocus autocomplete="name" placeholder="John Doe">
+        <input type="text" name="name" autofocus autocomplete="name" placeholder="John Doe" required>
         <label class="label" for="email">What is your email address?</label>
-        <input type="text" name="email" autocomplete="email" placeholder="johndoe@gmail.com">
+        <input type="text" name="email" autocomplete="email" placeholder="johndoe@gmail.com" required>
         <label class="label"  for="phone">What is your phone number?</label>
-        <input type="text" name="phone" autocomplete="tel" placeholder="555-555-5555">
+        <input type="text" name="phone" autocomplete="tel" placeholder="555-555-5555" required>
         <label class="label" for="message">What would you like to say to me?</label>
-        <textarea type="text" rows="4" name="message" placeholder="Hey, Robert. Nice website!"></textarea><br>
+        <textarea type="text" rows="4" name="message" placeholder="Hey, Robert. Nice website!" required></textarea><br>
         <input class="crispbutton" style="margin-top:3px" type="submit" value="Send Message">
         {{csrf_field()}}
     </form>
