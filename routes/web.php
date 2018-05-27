b5c54551e5af1e6b39fa80ada91a86e52bf64d89
Robert-Roy <robertproy@live.com>
2018-05-27 13:27:34 -0500

    improve routes to account for local development vs hosted page

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,32 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
-Route::get('/robertsworkspace/', 'HomeController@index');
-Route::get('/robertsworkspace/contact', 'ContactController@index');
-Route::get('/robertsworkspace/projects', 'ProjectsController@index');
-Route::get('/robertsworkspace/privacy', 'PrivacyController@index');
-Route::get('/robertsworkspace/github', 'GithubController@index');
-Route::get('/robertsworkspace/ipdata', 'IPDataController@index');
-Route::get('/robertsworkspace/dashboard', 'DashboardController@index');
-Route::post('/robertsworkspace/sendmail', 'SendmailController@index');
+if (Request::ip() === "::1") {
+    Route::get('/robertsworkspace/', 'HomeController@index');
+    Route::get('/robertsworkspace/contact', 'ContactController@index');
+    Route::get('/robertsworkspace/projects', 'ProjectsController@index');
+    Route::get('/robertsworkspace/privacy', 'PrivacyController@index');
+    Route::get('/robertsworkspace/github', 'GithubController@index');
+    Route::get('/robertsworkspace/ipdata', 'IPDataController@index');
+    Route::get('/robertsworkspace/dashboard', 'DashboardController@index');
+    Route::post('/robertsworkspace/sendmail', 'SendmailController@index');
+} else {
+    Route::get('', 'HomeController@index');
+    Route::get('/contact', 'ContactController@index');
+    Route::get('/projects', 'ProjectsController@index');
+    Route::get('/privacy', 'PrivacyController@index');
+    Route::get('/github', 'GithubController@index');
+    Route::get('/ipdata', 'IPDataController@index');
+    Route::get('/dashboard', 'DashboardController@index');
+    Route::post('/sendmail', 'SendmailController@index');
+}
