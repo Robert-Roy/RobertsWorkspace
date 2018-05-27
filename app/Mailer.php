379c11242bb4859d8635b81de3b89fdfe3699018
Robert-Roy <robertproy@live.com>
2018-05-27 15:00:34 -0500

    properly configure mailing settings

--- app/Mailer.php
+++ app/Mailer.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Request;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class Mailer {
 
@@ -10,12 +11,27 @@ class Mailer {
     //For now, this will have to do
 
     public function mailAdmin($subject, $message) {
-        if (Request::ip() === "::1") {
-            // don't send locally.
-            echo "attempted to send mail locally";
-        } else {
-           // mail(config('constants.ADMINEMAIL'), $subject, $message, "From: <" . config('constants.ADMINEMAIL') . ">");
+        // uncomment to disable mailing
+        //if (Request::ip() === "::1") {
+        //    // don't send locally.
+        //    echo "attempted to send mail locally";
+        //    return;
+        $mail = new PHPMailer(true);
+        $mail->SMTPDebug = 0; // increment to 2 for more detailed information
+        $mail->IsSMTP();
+        $mail->CharSet = 'utf-8';
+        $mail->Host = env('MAIL_HOST');
+        $mail->Port = env('MAIL_PORT');
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = env('MAIL_ENCRYPTION');
+        $mail->Username = env('MAIL_USERNAME');
+        $mail->Password = env('MAIL_PASSWORD');
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+        $mail->setFrom(config("constants.ADMINEMAIL"), "Robert");
+        $mail->addAddress(config("constants.ADMINEMAIL"), "Robert");
+        if (!$mail->send()) {
+            // TODO LOG ERROR
         }
     }
-
 }
