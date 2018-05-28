532c0a5075b9871b4593d69f6c709a40434904cb
Robert-Roy <robertproy@live.com>
2018-05-28 15:16:50 -0500

    update homepage view to better reflect experience gained

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,36 @@
 @section('content')
 
 <div class="contentdiv">
-    <p>I have used:</p>
+    <p>I have used the following languages:</p>
     <ul class='shrink-list'>
         <li>PHP</li>
+        <li>JavaScript</li>
+        <li>SQL</li>
         <li>Linux</li>
         <li>ASP</li>
         <li>Java</li>
-        <li>JavaScript</li>
         <li>Python</li>
-        <li>MySQL</li>
         <li>VB.NET</li>
-        <li>GIMP 2</li>
+    </ul>
+</div>
+
+<br>
+
+<div class="contentdiv">
+    <p>I have used the following APIs, libraries, frameworks, and softwares:</p>
+    <ul class='shrink-list'>
+        <li>KnockoutJS</li>
         <li>jQuery</li>
+        <li>MySQL</li>
+        <li>Linux</li>
+        <li>PHPMailer</li>
+        <li>GIMP 2</li>
         <li>WordPress</li>
+        <li>Google Maps API</li>
+        <li>Foursquare API</li>
+        <li>IP-API</li>
+        <li>cPanel</li>
+        <li>XAMPP</li>
     </ul>
 </div>
 
@@ -23,13 +40,15 @@
 
 <div class="contentdiv">
     <p>This website uses:</p>
-    <ul>
+    <ul class='shrink-list'>
         <li>PHP (<?= phpversion(); ?>)</li>
         <li>Laravel ({{ App::VERSION() }})</li>
         <li>JavaScript</li>
         <li>jQuery</li>
         <li>Git</li>
         <li>MySQL (PDO)</li>
+        <li>PHPMailer</li>
+        <li>IP-API</li>
     </ul>
 </div>
 
