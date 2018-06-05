d322778c0b0377f24c9e9d4c3736829cd55ff733
Robert-Roy <robertproy@live.com>
2018-06-05 15:39:35 -0500

    FEATURE: Modify constants.php so that all href links now include site environment URL

--- config/constants.php
+++ config/constants.php
@@ -2,25 +2,26 @@
 
 return [
     //pages
-    "ANALYTICS" => "analytics",
-    "PRIVACY" => "privacy",
-    "PROJECTS" => "projects",
-    "HOME" => ".",
-    "CONTACT" => "contact",
-    "ROBERTSANALYTICS" => "dashboard",
-    "IPDATA" => "ipdata",
-    "ADMIN" => "admin",
-    "GITHUB" => "github",
+    "ANALYTICS" => env('APP_URL') . "analytics",
+    "PRIVACY" => env('APP_URL') . "privacy",
+    "PROJECTS" => env('APP_URL') . "projects",
+    "HOME" => env('APP_URL'),
+    "CONTACT" => env('APP_URL') . "contact",
+    "ROBERTSANALYTICS" => env('APP_URL') . "dashboard",
+    "IPDATA" => env('APP_URL') . "ipdata",
+    "ADMIN" => env('APP_URL') . "admin",
+    "GITHUB" => env('APP_URL') . "github",
     //Resources
-    "UNDERCONSTRUCTION" => "public_html/images/sign.png",
-    "MAINSCRIPT" => "public/js/script.js",
-    "ANIMSCRIPT" => "public/js/anim.js",
-    "CSS" => "public/css/default.css",
+    "UNDERCONSTRUCTION" => env('APP_URL') . "public_html/images/sign.png",
+    "MAINSCRIPT" => env('APP_URL') . "public/js/script.js",
+    "ANIMSCRIPT" => env('APP_URL') . "public/js/anim.js",
+    "CSS" => env('APP_URL') . "/public/css/default.css",
+    "ADMINCSS" => env('APP_URL') . "/public/css/admin.css",
     "ICO" => "favicon.ico",
     "SQLCONNECTOR" => "core/sql/sqlconnector.php",
     //Information
     "PHONE" => "555-555-5555",
     "SITENAME" => "Robert's Workspace",
     "ADMINEMAIL" => "robert@robertsworkspace.com",
-    "FAVICONROOT" => "public/favicon/"
+    "FAVICONROOT" => env('APP_URL') . "public/favicon/"
 ];
